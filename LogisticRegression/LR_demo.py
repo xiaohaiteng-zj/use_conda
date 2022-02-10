124ab5fd1054d5e67a137eae96a1811813cb4011
jia.zhang@aiseeding.com <jia.zhang@aiseeding.com>
2022-02-10 15:15:47 +0800

    fea: LogisticRegression逻辑回归分类算法

---
+++ LogisticRegression/LR_demo.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2022/2/9 23:40
+# @Author  : 小海腾
+import pandas as pd
+
+re = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.data")
+print(re)
